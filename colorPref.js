d7060a12e06a45171dd2c646059687e5348b9647
Christine <guerrerochristinejoy15@gmail.com>
2024-11-04 22:26:14 +0800

    colorPref.js and index.html

---
+++ colorPref.js
@@ -0,0 +1,15 @@
+function colorPref() {
+    let favColor = [];
+
+    let addColor = prompt("Enter a color you like:");
+    favColor.push(addColor);
+        alert("Color Preferences: "+ favColor + " ")
+
+    let addColor2= prompt("Enter a color you like:");
+    favColor.push(addColor2);
+        alert("Color Preferences: " + favColor + " ")
+
+    let addColor3 = prompt("Enter a color you like:");
+    favColor.push(addColor3);
+        alert("Color Preferences: " + favColor + " ")
+}
